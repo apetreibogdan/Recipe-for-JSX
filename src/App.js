39357f269148a0fdcc76ef444e227eacf0856b51
apetreibogdan <70704260+apetreibogdan@users.noreply.github.com>
2021-12-29 14:07:37 +0200

    Preparation with ingredients highliter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import perfectionSaladImg from './images/perfection_salad.jpg';
-
+import React from "react";
+import Highlighter from "react-highlight-words";
 
 function App() {
   const ingredients = [
@@ -49,16 +50,49 @@ function App() {
       quantity: "1"
     }
   ]
-  console.log(ingredients)
-  
+// function GetIngredientsNameList() {
+//   let ingredientsNameList=[];
+//    ingredients.map(ingredient=>(
+//     ingredientsNameList.push(ingredient.name)
+//   ));
+//   return console.log(ingredientsNameList);
+// }
+const ingredientsNameList=
+[
+  "unflavored gelatine","sugar","salt","apple juice","lemon juice","vinegar","carrot","celery","cabbage","green pepper","pimiento"
+];
+
   return (
     <div className='container'>
-        <h1>Grandma's recipes</h1>
-        <img src={perfectionSaladImg} alt="Perfection Salad" />
-        <h2>Ingredients</h2>
-        <ul>
-          {ingredients.map(ingredient =>(<li>{ingredient.quantity} {ingredient.name}</li>))}
-        </ul>
+      <h1>Grandma's recipes</h1>
+      <img src={perfectionSaladImg} alt="Perfection Salad" />
+      <h2>Ingredients</h2>
+      <ul>
+        {ingredients.map(ingredient => (
+          <li key={ingredient.name}>
+          <span className='liQuantity'>{ingredient.quantity} </span>
+             {ingredient.name}
+          </li>))}
+      </ul>
+      <h2>Preparation</h2>
+      <Highlighter
+    highlightClassName="YourHighlightClass"
+    searchWords={ingredientsNameList}
+    autoEscape={true}
+    textToHighlight="1. In small saucepan, combine gelatine, sugar, and salt; mix well.<br/>
+
+    2. Add 1 cup water. Heat over low heat, stirring constantly, until sugar and gelatine are dissolved. Remove from heat.
+    
+    3. Stir in apple juice, lemon juice, vinegar, and 1/4 cup cold water. Pour into medium bowl. Refrigerate 1 hour, or until mixture is consistency of unbeaten egg white.
+    
+    4. Add carrot, celery, cabbage, green pepper, and pimiento; stir until well combined.
+    
+    5. Turn into decorative, 1 1/2-quart mold. Refrigerate 4 hours,or until firm.
+    
+    6. To unmold: Run small spatula around edge of mold; invert onto serving plate. Place hot dishcloth over mold; shake gently to release. Repeat, if necessary. Lift off mold. refrigerate until ready to serve.
+    
+    Makes 8 servings."
+  />
     </div>
   );
 }
